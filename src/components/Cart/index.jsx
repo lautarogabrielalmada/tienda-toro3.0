82151e172d811ea3195d474f82e5cc0b865a2449
lautaroalmada <lautarogabrielalmada@gmail.com>
2022-12-31 15:03:28 -0300

    Se crea orden en firestore

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,3 +1,4 @@
+import { addDoc, getFirestore, collection } from "firebase/firestore";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
@@ -6,6 +7,28 @@ import ItemCart from "../ItemCart";
 const Cart = () => {
 
     const {cart, totalPrice} = useCartContext();
+
+
+const order = {
+    buyer:{
+        name:"Jose",
+        email:"jose@gmail.com",
+        phone:"1164350226",
+        adress:"asdd"
+
+    },
+    items:cart.map(productos => ({id:productos.id, nombre: productos.nombre, precio: productos.precio, quantity: productos.quantity})),
+    total:totalPrice(),
+}
+
+const handleClick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db,"orders");
+
+    addDoc(ordersCollection, order)
+    .then(({id})=> console.log(id))
+
+}
 
     if(cart.length === 0){
         return(
@@ -23,6 +46,8 @@ const Cart = () => {
             cart.map(product => <ItemCart key={product.id} product={product}/>)
         }
         <p>total: $ {totalPrice()}</p>
+
+        <button onClick={handleClick}>Emitir compra</button>
         </>
     )
 }
